76e9cdb87956b8541199673d2d46e2f41b3362e5
Arheo8 <78500348+Arheo8@users.noreply.github.com>
2023-02-14 03:08:30 +0530

    First commitgit add .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const taskInput = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const task = {
+      id: tasks.length + 1,
+      title: taskInput.current.value,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, task]);
+    taskInput.current.value = "";
+  };
+
+  const handleCheckboxChange = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+      )
+    );
+  };
+
+  const handleClearButton = () => {
+    setTasks(tasks.filter((task) => !task.isCompleted));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To-Do App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={taskInput} />
+        <button type="submit">Add Task</button>
+        <button type="button" onClick={handleClearButton}>
+          Clear Completed Tasks
+        </button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li
+            key={task.id}
+            style={{
+              textDecoration: task.isCompleted ? "line-through" : "none",
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={task.isCompleted}
+              onChange={() => handleCheckboxChange(task.id)}
+            />
+            {task.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
